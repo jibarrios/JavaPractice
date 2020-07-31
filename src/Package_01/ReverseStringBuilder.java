a9c373371c2bb440a8141358b2bc387df8ff2632
Yourname <jibarrios>
2020-07-31 15:52:50 -0400

    reverse

---
+++ src/Package_01/ReverseStringBuilder.java
@@ -0,0 +1,15 @@
+package Package_01;
+
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReverseStringBuilder {
+
+    public static void main(String[] args) {
+        StringBuilder Str = new StringBuilder("greeting");
+        Str.reverse();
+        System.out.println(Str);
+
+    }
+}
---
+++ src/Package_01/Reverse_02.java
@@ -0,0 +1,17 @@
+package Package_01;
+
+public class Reverse_02 {
+    public static void main(String[] args) {
+
+    String word ="string";
+        String reverse ="";
+
+        for (int i = word.length()-1; i>= 0;  i--) {
+            reverse+= word.charAt(i);
+
+
+
+
+        }
+        System.out.println(reverse);
+        }}
